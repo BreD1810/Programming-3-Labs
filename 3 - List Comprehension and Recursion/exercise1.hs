df8012de2f263cbf9318c0932774fdf92659b805
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2018-10-23 11:33:28 +0100

    Fixed exercise 1

--- 3 - List Comprehension and Recursion/exercise1.hs
+++ 3 - List Comprehension and Recursion/exercise1.hs
@@ -1,2 +1,2 @@
 -- Sum of the squares of odd numbers and cubes of even numbers for the first 100 ints
-sum [ x^2 | x <- [0, 2..100] ] + sum [y^3 | y <- [1, 3..100]]
+sum [ x^2 | x <- [1, 3..100] ] + sum [y^3 | y <- [0, 2..100]]
