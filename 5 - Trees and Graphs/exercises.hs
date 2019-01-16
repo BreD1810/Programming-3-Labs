de5ac81c1ea1074bbe9b2f427024acce7ab5267e
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2019-01-16 11:08:23 +0000

    Added solution for sheet 5 exercise 2

--- 5 - Trees and Graphs/exercises.hs
+++ 5 - Trees and Graphs/exercises.hs
@@ -1,3 +1,5 @@
+testTree = Node (Node (Leaf 1) 2 (Leaf 3)) 4 (Leaf 5)
+
 -- Exercise 1
 data Tree a = Leaf a | Node (Tree a) a (Tree a)
 
@@ -9,4 +11,10 @@ occurs a (Node left b right) | result == EQ = True
     where
         result = compare a b
 
-testTree = Node (Node (Leaf 1) 2 (Leaf 3)) 4 (Leaf 5)
+-- Exercise 2
+foldTree :: (a -> b) -> (a -> b -> b -> b) -> Tree a -> b
+foldTree leaff nodef (Leaf x) = leaff x
+foldTree leaff nodef (Node left x right) = nodef x (foldTree leaff nodef left) (foldTree leaff nodef right)
+
+flatten :: Tree a -> [a]
+flatten = foldTree (\x -> [x]) (\x ls rs -> ls ++ (x:rs))
